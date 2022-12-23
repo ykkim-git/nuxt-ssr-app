7922399aa9c854f712b6f284c001ed4bced0a4d7
jay <rladudrnjs12@gmail.com>
2022-12-23 18:51:18 +0900

    express set test api

--- api/index.js
+++ api/index.js
@@ -3,9 +3,12 @@ const express = require('express')
 // express 인스턴스 생성
 const app = express()
 
-// 실제로는 /api 라우트를 처리하는 메소드가 된다.
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.send('API root test! !')
+})
+
+app.get('/testapi', (req, res) => {
+  res.send('this is test api ! hello world :)')
 })
 
 module.exports = {
